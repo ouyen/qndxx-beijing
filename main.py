2d438e8c764905bb29609171bbbc0b91b949c400
ouyen <ouyen@users.noreply.github.com>
2021-11-05 13:35:21 +0800

    change default org_id

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class QNDXX_NEW_COURSE():
         self.id = 59
         self.title = '青年大学习：江山就是人民，人民就是江山'
         self.url = "https://h5.cyol.com/special/daxuexi/byw1m1kn1s/m.html?t=1&z=201",
-        self.org_id = 172440  #"北京市东城团区委"
+        self.org_id = 172442  #"北京市海淀团区委"
         self.end_img_url = 'https://h5.cyol.com/special/daxuexi/byw1m1kn1s/images/end.jpg'  #example
         self.study_url= "https://m.bjyouth.net/dxx/check?id=%s&org_id=%s" % (
             self.id, self.org_id
@@ -31,7 +31,7 @@ class QNDXX_NEW_COURSE():
             self.id = index['newCourse']['id']
             self.title = index['newCourse']['title']
             self.url = index['newCourse']['url']
-            self.org_id = index['rank'][0]['data'][1]['org_id']
+            # self.org_id = index['rank'][0]['data'][1]['org_id']
             i = self.url.find("/m.html")
             self.end_img_url = self.url[:i] + '/images/end.jpg'
             self.study_url= "https://m.bjyouth.net/dxx/check?id=%s&org_id=%s" % (
